34f744336ae0f4b1c5be1d54c79ee06367e2c609
stefanavra <stefan.avra@gmail.com>
2021-01-17 23:19:42 +0100

    flask migrate

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
 
 from app.forms import TinyForm
 
--- app/models.py
+++ app/models.py
@@ -5,5 +5,9 @@ from datetime import datetime
 class TinyText(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(32*24))
+    title = db.Column(db.String(32))
     created = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     votes = db.Column(db.Integer, default=0)
+
+    def __repr__(self):
+        return '<TinyText id:{}, created:{}, title:{}, votes:{}>'.format(self.id, self.created, self.title, self.votes)    
