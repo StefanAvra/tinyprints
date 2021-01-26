814f15c89c45f57467174c430c36e0cd87a80ee9
stefanavra <stefan.avra@gmail.com>
2021-01-26 20:08:35 +0100

    changed posts per page to 12

--- config.py
+++ config.py
@@ -8,8 +8,7 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    POSTS_PER_PAGE = 4
+    POSTS_PER_PAGE = 12
     AUTH_SECRET = os.environ.get('AUTH_SECRET') or 'dev'
 
 
-    
